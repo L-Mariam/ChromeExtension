e3945adc9211cd5855619710517644303e5995d0
L-Mariam <l.mariam.here@gmail.com>
2024-06-03 22:45:54 +0400

    Prevent missing lazy loading images
    - changes all images once the DOM is fully loaded using 'load' event listener
    - 'MutationObserver is used to monitor changes in the DOM. If new images are added, the script will change their src attributes as well

--- Content.js
+++ Content.js
@@ -5,11 +5,31 @@ let bobRossImages = [
     "https://bit.ly/3osrfoi",
     "https://bit.ly/3qCPjax",
     "https://bit.ly/3CkRXE6",
+    "https://bit.ly/3CkRXE6",
+    "https://bit.ly/3CkRXE6",
+    "https://bit.ly/3CkRXE6",
+    "https://bit.ly/3CkRXE6",
   ];
   
-  const imgs = document.getElementsByTagName("img");
   
-  for (image of imgs) {
-    const index = Math.floor(Math.random() * bobRossImages.length);
-    image.src = bobRossImages[index];
-  }
+function changeImages() {
+    const imgs = document.getElementsByTagName("img");
+    for (let image of imgs) {
+        const index = Math.floor(Math.random() * bobRossImages.length);
+        image.src = bobRossImages[index];
+    }
+}
+
+// Initial change of images when the DOM is fully loaded
+window.addEventListener('load', changeImages);
+
+// Observe changes in the DOM to catch lazy-loaded or dynamically added images
+const observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+        if (mutation.type === 'childList') {
+            changeImages();
+        }
+    }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
